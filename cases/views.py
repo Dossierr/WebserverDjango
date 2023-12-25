f5fc2a58a42c3867c5ed250ae1957f661543ebc6
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2023-12-25 12:01:45 +0100

    added task que for reindexing

--- cases/views.py
+++ cases/views.py
@@ -2,6 +2,17 @@ from rest_framework import serializers, viewsets
 from .models import Case, File
 from .permissions import IsOwnerOrReadOnly, CommonAuthenticationMixin
 from .serializers import CaseSerializer, FileSerializer
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from django.utils import timezone
+import requests
+from rq import Queue
+from core.settings import redis_client
+
+
+def reindex_files(case_id):
+    pass
 
 # Viewsets
 class CaseViewSet(CommonAuthenticationMixin, viewsets.ModelViewSet):
@@ -48,3 +59,37 @@ class FileViewSet(CommonAuthenticationMixin, viewsets.ModelViewSet):
             # Handle the case where the user is not authenticated
             # You might want to raise an exception or handle it differently
             pass
+        
+    @action(detail=False, methods=['post'])
+    def reindex(self, request):
+        """
+        Reindexes the files in a case
+        """
+        user = self.request.user
+
+        # Check if the user is authenticated
+        if not user.is_authenticated:
+            return Response({"error": "Authentication required"}, status=401)
+
+        # Store the request body before accessing request.data
+        request_body = request.body
+
+        # Get the case_id from the request data
+        case_id = request.data.get('case')
+
+        # Check if case_id is provided
+        if not case_id:
+            return Response({"error": "Case ID is required"}, status=400)
+
+        # Get the case object or return 404 if not found
+        case = get_object_or_404(Case, id=case_id, user=user)
+        case_id = str(case.id)
+
+        # Add your reindexing logic here using the case object
+        File.objects.filter(case=case).update(updated_at=timezone.now())
+        # Enqueue the task
+        queue = Queue(connection=redis_client)
+        result = queue.enqueue(reindex_files, case_id)
+
+
+        return Response({"message": f"Task {result.id} enqueued for reindexing files in Case {case_id}"}, status=200)
